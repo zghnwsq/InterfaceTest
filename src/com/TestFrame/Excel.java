b25af319b74d27fb746975b352914d3a810dc492
ted.wang <tedwang@sina.cn>
2018-11-15 21:30:37 +0800

    增加写入excel方法

--- src/com/TestFrame/Excel.java
+++ src/com/TestFrame/Excel.java
@@ -1,10 +1,18 @@
 package com.TestFrame;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -58,6 +66,42 @@ public class Excel {
         }
         return lists;
     }
+    
+    //写入excel
+    public static void writeExcel(ArrayList<ArrayList<String>> result,String path){  
+        if(result == null){  
+            return;  
+        }  
+        HSSFWorkbook wb = new HSSFWorkbook();  
+        HSSFSheet sheet = wb.createSheet("sheet1");  
+        for(int i = 0 ;i < result.get(0).size() ; i++){  
+            HSSFRow row = sheet.createRow(i);  
+                for(int j = 0; j < result.size() ; j ++){
+                    HSSFCell cell = row.createCell((short)j);
+                    cell.setCellValue(result.get(j).get(i).toString());  
+                }  
+        }  
+        ByteArrayOutputStream os = new ByteArrayOutputStream();  
+        try  
+        {  
+            wb.write(os);  
+        } catch (IOException e){  
+            e.printStackTrace();  
+        }  
+        byte[] content = os.toByteArray();  
+        File file = new File(path);//Excel文件生成后存储的位置。  
+        OutputStream fos  = null;  
+        try  
+        {  
+            fos = new FileOutputStream(file);  
+            wb.write(fos);  
+            os.close();  
+            fos.close();
+            wb.close();
+        }catch (Exception e){  
+            e.printStackTrace();  
+        }             
+    } 
 
 
 }
